5f01e46ffe1bd3f0024fc3051e3cfae02a0451a6
Manish Goregaokar <manishearth@gmail.com>
2017-02-22 17:19:04 -0800

    Bug 1329093 - Part 4: stylo: Delay SVG mapped attr resolution till later;
    MozReview-Commit-ID: 2GvHPg1egjS

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -186,6 +186,8 @@ ServoStyleSet::ResolveMappedAttrDeclarationBlocks()
   if (nsHTMLStyleSheet* sheet = mPresContext->Document()->GetAttributeStyleSheet()) {
     sheet->CalculateMappedServoDeclarations();
   }
+
+  mPresContext->Document()->ResolveScheduledSVGPresAttrs();
 }
 
 bool
